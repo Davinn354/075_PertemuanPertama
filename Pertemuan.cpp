d1d9e8259bea489047bea78d121ecd1f5e5d7150
Davinn354 <daviipad53@gmail.com>
2024-02-26 15:16:26 +0700

    Add project files.

---
+++ Pertemuan.cpp
@@ -0,0 +1,25 @@
+// Pertemuan.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{//begin
+    // numeric Panjag, nLebar, Luas
+    // display 'Masukan Pajang= '
+    //accept nPanjang
+    //display 'masukan Lebar = ''
+    //accept nLebar
+    //compute nLuas = nPanjang * nLebar
+    //display 'Luasnya adalah = ' + nLuas
+
+    int nPanjang, nLebar, nLuas;
+    cout << " Masukan Pannjang = ";
+    cin >> nPanjang;
+    cout << "Masukan Lebar = ";
+    cin >> nLebar;
+    nLuas = nPanjang* nLebar;
+    cout << " Luasnya adalah " nLuas << endl
+
+    system("pause");
+}
